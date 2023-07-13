c5829ded5eafff3a0f2ffe6a7d1ae10d1ab8a029
Ivan Mosteo <ivan.mosteo@ideamail.es>
2023-07-13 12:42:25 +0200

    Fix error fondo pantalla color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
-<h1 class="text-3xl font-bold underline">
+<h1 class="text-3xl font-bold ml-5 my-3 underline">
     Hello world!
-  </h1>
+</h1>
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-5">
+    Click me
+</button>
   
-  <style lang="postcss">
+<style lang="postcss">
     :global(html) {
-      background-color: theme(colors.gray.100);
+        background-color: theme("colors.gray.100");
     }
-  </style>
+</style>
